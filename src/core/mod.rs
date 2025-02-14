edc5874602cef735f54ec9766f998294166b5c3a
Aleksei Bavshin <a.bavshin@nginx.com>
2025-02-14 10:25:38 -0800

    feat: add `ngx_container_of!` macro

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -7,3 +7,16 @@ pub use buffer::*;
 pub use pool::*;
 pub use status::*;
 pub use string::*;
+
+/// Gets an outer object pointer from a pointer to one of its fields.
+/// While there is no corresponding C macro, the pattern is common in the NGINX source.
+///
+/// # Safety
+///
+/// `$ptr` must be a valid pointer to the field `$field` of `$type`.
+#[macro_export]
+macro_rules! ngx_container_of {
+    ($ptr:expr, $type:path, $field:ident) => {
+        $ptr.byte_sub(::core::mem::offset_of!($type, $field)).cast::<$type>()
+    };
+}
